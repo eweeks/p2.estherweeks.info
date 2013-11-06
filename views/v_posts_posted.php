76a4269f20e4dc0400527ce4157154a7a6fbab49
Esther Weeks <esta.weeks@gmail.com>
2013-11-05 21:38:05 -0700

    modified:   views/v_posts_posted.php -deleted stray <li> tag

--- views/v_posts_posted.php
+++ views/v_posts_posted.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-md-12 col-sm-12">
 		<h1>Your post was added</h1>
-		<a href="/posts/edit">View Your Posts</a></li>
+		<a href="/posts/edit">View Your Posts</a>
 	</div><!-- end class col-->
 </div><!--end class row -->
